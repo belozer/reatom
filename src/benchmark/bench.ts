158f91bf78f82ccdf2fdd9c8f3aed90238be3150
Sergey Belozyorcev <belozyorcev@ya.ru>
2019-08-31 04:11:08 +0300

    up

--- src/benchmark/bench.ts
+++ src/benchmark/bench.ts
@@ -1,42 +1,71 @@
 import { performance } from 'perf_hooks'
 
-const measunres: { [key: string]: number[] } = {}
+const samplePerc = 0.95
+const samples = 4
+const iterations = 1000
 
 interface Options {
-    samples?: number,
-    before?: () => any,
-    after?: () => any
+  before?: () => any,
+  after?: (payload: any) => void
 }
+
+type TestFunc = (payload: any) => void
+type Measurements = number[]
 
 const getMax = (numArray: number[]) => Math.max.apply(null, numArray)
 const getMin = (numArray: number[]) => Math.min.apply(null, numArray)
 
-export const bench = (id: string, fn: (payload: any) => void, opts?: Options) => {
+export const bench = (id: string, fn: TestFunc, opts?: Options) => {
+  const measurements: Measurements = []
+  const errors: number[] = []
+
+  for (let i = 0; i < samples; i++) {
+    const { mid, error } = sample(fn, opts);
+    errors.push(error)
+    measurements.push(mid)
+  }
+
+  const { min, max, mid, error } = calc(measurements)
+
+  // console.log(measunres[id])
+  // const midError = errors.reduce((acc, curr) => acc + curr, 0) / errors.length
+
+  const opSec = 1000 / mid
+
+  // console.log(id, `${mid.toFixed(3)}ms`)
+  // console.log(id, `${mid.toFixed(3)}ms`, `± ${error.toFixed(2)}%`)
+  // console.log(id, `${opSec.toFixed(0)} ops/sec`)
+  console.log(id, `× ${opSec.toFixed(0)} ops/sec`, `± ${error.toFixed(2)}%`)
+}
+
+const sample = (fn: TestFunc, opts?: Options) => {
+  const measurements: Measurements = []
+
   const options = {
-    samples: 10000,
     before: () => {},
     after: () => {},
     ...opts
   }
 
-  if (measunres[id] === undefined) measunres[id] = [];
-
-  for (let i = 0; i < options.samples; i++) {
-    const payload = options.before();
-    const start = performance.now();
-    fn(payload);
-    measunres[id].push(performance.now() - start)
+  for (let i = 0; i < iterations; i++) {
+    const payload = options.before()
+    const start = performance.now()
+    fn(payload)
+    measurements.push(performance.now() - start)
+    options.after(payload)
   }
 
-  const min = getMin(measunres[id]);
-  const max = getMax(measunres[id]);
-  const mid = measunres[id].reduce((acc, curr) => acc + curr, 0) / measunres[id].length;
+  return calc(measurements)
+}
 
-  // console.log(measunres[id].sort((a, b) => a - b))
-  // console.log(measunres[id])
-  const errorPerc = getMax([mid - min, max - mid]) / mid * 100;
+const calc = (measurements: number[]): { mid: number, max: number, min: number, error: number } => {
+  const trimSize = Math.round(measurements.length * (1 - samplePerc))
+  const filtered = measurements.slice(trimSize / 2 - 1, measurements.length - trimSize)
 
-  const opSec = 1000 / mid;
+  const min = getMin(filtered);
+  const max = getMax(filtered);
+  const mid = filtered.reduce((acc, curr) => acc + curr, 0) / filtered.length
+  const error = getMax([mid - min, max - mid]) / mid * 100
 
-  console.log(id, `× ${opSec.toFixed(2)} ops/sec`, `± ${errorPerc.toFixed(2)}%`);
+  return { mid, max, min, error }
 }
